f7915861c27fcf3f6b4c7e1b3e1715a27db0d4a4
demo <demo@gmail.com>
2025-08-04 13:10:41 +0530

    Day 4: Added Member model and registered it in admin

--- members/models.py
+++ members/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    membership_start = models.DateField()
+    membership_end = models.DateField()
+    plan_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
